9ac7699b603dd359a88ac86c1f794672f7cb933f
Thomas So <thomas.m.so33@gmail.com>
2017-04-12 22:48:28 -0700

    Add AdMob banner support for iOS

---
+++ AdMob/iOS/AppLovinCustomEventBanner.h
@@ -0,0 +1,14 @@
+//
+//  AppLovinCustomEventBanner.h
+//
+//
+//  Created by Thomas So on 4/12/17.
+//
+//
+
+#import <Foundation/Foundation.h>
+@import GoogleMobileAds;
+
+@interface AppLovinCustomEventBanner : NSObject <GADCustomEventBanner>
+
+@end
